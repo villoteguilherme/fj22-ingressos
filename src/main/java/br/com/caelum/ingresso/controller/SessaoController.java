8ed50838537bcdbe4a40aed7282d5ef247595564
Guilherme Villote <villote.guilherme@eb.mil.br>
2021-04-15 14:29:06 +0000

    Carrinho

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -25,6 +25,7 @@ import br.com.caelum.ingresso.model.Sessao;
 import br.com.caelum.ingresso.model.ImagemCapa;
 import br.com.caelum.ingresso.model.TipoDeIngresso;
 import br.com.caelum.ingresso.model.form.SessaoForm;
+import br.com.caelum.ingresso.model.Carrinho;
 import br.com.caelum.ingresso.validacao.GerenciadorDeSessao;
 import br.com.caelum.ingresso.rest.OmdbClient;
 
@@ -42,6 +43,9 @@ public class SessaoController {
     
     @Autowired
     private OmdbClient client;
+    
+    @Autowired
+    private Carrinho carrinho;
     
     @GetMapping("/admin/sessao")
     public ModelAndView form(@RequestParam("salaId") Integer salaId) {
@@ -84,9 +88,9 @@ public class SessaoController {
         Sessao sessao = sessaoDao.findOne(sessaoId);
         Optional<ImagemCapa> imagemCapa = client.request(sessao.getFilme(), ImagemCapa.class);
         modelAndView.addObject("sessao", sessao);
+        modelAndView.addObject("carrinho", carrinho);
         modelAndView.addObject("imagemCapa", imagemCapa.orElse(new ImagemCapa()));
         modelAndView.addObject("tiposDeIngressos", TipoDeIngresso.values());
         return modelAndView;
     }
-
 }
